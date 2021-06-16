889beee2427b0e60e6c9d9b745dc416396edd179
Samruddhi Khandale <khandale.samruddhi@gmail.com>
2021-06-16 20:28:21 +0000

    test

--- run-me.sh
+++ run-me.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-
+echo "test1"
 echo "::step::Running child step 1"
+echo "test2"
 echo -n "I was triggered by the postcreate" >> ./child1.txt
 sleep 30s
 echo "::step::Step 2"
 echo -n "I was triggered by the postcreate" >> ./child2.txt
 sleep 30s
+echo "test3"
 echo "::endstep::"
+echo "test4"
 echo -n "I was triggered by the postcreate" >> ./childEndStep.txt
 sleep 30s
+echo -n "test2"
 echo "::step::3rd step"
+echo -n "test2"
 echo -n "I was triggered by the postcreate" >> ./child3.txt
 sleep 30s
